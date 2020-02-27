ee9fd8e44c9142a2ebcaf681aaf2a18cf7bdb9ee
Heitor Chang <heitorchang@gmail.com>
2020-02-27 16:05:06 -0300

    read accounts

--- alexieui/views.py
+++ alexieui/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from acct.models import AcctType, Acct, Txn
 
 
 def index(request):
-    return render(request, 'alexieui/base.html')
+    atypes = AcctType.objects.all()
+
+    for atype in atypes:
+        atype.accts = Acct.objects.filter(user=request.user, acctType=atype)
+        
+    ts = Txn.objects.filter(user=request.user)
+    
+    return render(request, 'alexieui/index.html',
+                  {'atypes': atypes,
+                   'ts': ts})
