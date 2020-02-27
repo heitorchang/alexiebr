6c49e1cd01f9279aabe51b9d17b035c033ed0c3f
Heitor Chang <heitorchang@gmail.com>
2020-02-27 16:24:49 -0300

    summarize all

--- alexieui/views.py
+++ alexieui/views.py
@@ -1,15 +1,34 @@
+from collections import OrderedDict
 from django.shortcuts import render
 from acct.models import AcctType, Acct, Txn
 
 
 def index(request):
-    atypes = AcctType.objects.all()
+    atypes = OrderedDict()
+    accts = OrderedDict()
 
-    for atype in atypes:
-        atype.accts = Acct.objects.filter(user=request.user, acctType=atype)
+    # Initialize dict of account types
+    for atype in AcctType.objects.all():
+        atypes[atype.id] = atype
+        atypes[atype.id].accts = []
+
+    # assign an account to dict of all accounts
+    for acct in Acct.objects.filter(user=request.user):
+        accts[acct.id] = acct
+        accts[acct.id].bal = 0
         
-    ts = Txn.objects.filter(user=request.user)
+    # alter balances
+    for t in Txn.objects.filter(user=request.user):
+        dracct = accts[t.debit.id]
+        cracct = accts[t.credit.id]
+        
+        dracct.bal += t.amt * dracct.acctType.sign
+        cracct.bal -= t.amt * cracct.acctType.sign
+
+    # place account into its account type's list
+    for acct in accts.values():
+        atypes[acct.acctType.id].accts.append(acct)
+        
     
     return render(request, 'alexieui/index.html',
-                  {'atypes': atypes,
-                   'ts': ts})
+                  {'atypes': atypes})
