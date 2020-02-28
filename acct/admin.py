40e36e29275f17a2609eb84a296e10607963f143
Heitor Chang <heitorchang@gmail.com>
2020-02-28 15:35:33 -0300

    enable txn search

--- acct/admin.py
+++ acct/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import AcctType, Acct, Txn, Preset, HeaderBal
 
-admin.site.register([AcctType, Acct, Txn, Preset, HeaderBal])
+class TxnAdmin(admin.ModelAdmin):
+    ordering = ['-id']
+    search_fields = ['desc']
+    
+
+admin.site.register([AcctType, Acct, Preset, HeaderBal])
+admin.site.register(Txn, TxnAdmin)
