a5330207d979a57a470be4034956d531c03bb15c
Heitor Chang <heitorchang@gmail.com>
2020-04-14 18:46:10 -0300

    test index view

--- acct/tests.py
+++ acct/tests.py
@@ -1,3 +1,56 @@
+from decimal import Decimal
+from datetime import date
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import AcctType, Acct, Txn
 
-# Create your tests here.
+
+class AcctTypeTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='heitor',
+                                        email='heitorpontual@gmail.com',
+                                        password='secret')
+
+        
+    def test_acct_type_creation(self):
+        at = AcctType.objects.create(name='assets',
+                                     sign=1, order=1)
+        self.assertEqual(len(AcctType.objects.all()), 1)
+
+
+    def test_acct_creation(self):
+        at = AcctType.objects.create(name='assets',
+                                     sign=1, order=1)
+
+        a = Acct.objects.create(user=self.user,
+                                acctType=at,
+                                name='bank',
+                                budget=Decimal("0.00"))
+
+        self.assertEqual(len(Acct.objects.all()), 1)
+
+
+    def test_txn_creation(self):
+        at = AcctType.objects.create(name='assets',
+                                     sign=1, order=1)
+        
+        bank = Acct.objects.create(user=self.user,
+                                   acctType=at,
+                                   name='bank',
+                                   budget=Decimal("0.00"))
+        
+        wallet = Acct.objects.create(user=self.user,
+                                     acctType=at,
+                                     name='wallet',
+                                     budget=Decimal("0.00"))
+
+        xfer = Txn.objects.create(user=self.user,
+                                  date=date(2020, 3, 1),
+                                  desc="transfer",
+                                  amt=Decimal("100.00"),
+                                  debit=wallet,
+                                  credit=bank)
+        
+        self.assertEqual(len(Txn.objects.all()), 1)
+        
--- alexieui/tests.py
+++ alexieui/tests.py
@@ -1,3 +1,94 @@
+from decimal import Decimal
+from datetime import date
 from django.test import TestCase
+from django.contrib.auth.models import User
+from acct.models import AcctType, Acct, Txn
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(username='admin',
+                                                  email='admin@alexiebr.com',
+                                                  password='secret')
+
+        
+    def test_redirects_to_login(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 302)
+
+        
+    def test_uses_home_template(self):
+        self.client.login(username='admin', password='secret')
+        
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'alexieui/index.html')
+        
+
+    def test_shows_zero_balance(self):
+        self.client.login(username='admin', password='secret')
+
+        at = AcctType.objects.create(name='assets',
+                                     sign=1, order=1)
+        
+        bank = Acct.objects.create(user=self.user,
+                                   acctType=at,
+                                   name='bank',
+                                   budget=Decimal("0.00"))
+
+        response = self.client.get('/')
+        self.assertContains(response, "0,00")
+
+
+    def test_recent_txn_affects_balance(self):
+        self.client.login(username='admin', password='secret')
+
+        at = AcctType.objects.create(name='assets',
+                                     sign=1, order=1)
+        
+        bank = Acct.objects.create(user=self.user,
+                                   acctType=at,
+                                   name='bank',
+                                   budget=Decimal("0.00"))
+
+        wallet = Acct.objects.create(user=self.user,
+                                     acctType=at,
+                                     name='wallet',
+                                     budget=Decimal("0.00"))
+
+        xfer = Txn.objects.create(user=self.user,
+                                  date=date.today(),
+                                  desc="transfer",
+                                  amt=Decimal("123.00"),
+                                  debit=wallet,
+                                  credit=bank)
+
+        response = self.client.get('/')
+        self.assertContains(response, "123,00")
+
+
+    def test_old_txn_does_not_appear(self):
+        self.client.login(username='admin', password='secret')
+
+        at = AcctType.objects.create(name='assets',
+                                     sign=1, order=1)
+        
+        bank = Acct.objects.create(user=self.user,
+                                   acctType=at,
+                                   name='bank',
+                                   budget=Decimal("0.00"))
+
+        wallet = Acct.objects.create(user=self.user,
+                                     acctType=at,
+                                     name='wallet',
+                                     budget=Decimal("0.00"))
+
+        xfer = Txn.objects.create(user=self.user,
+                                  date=date(2001, 9, 9),
+                                  desc="transfer",
+                                  amt=Decimal("123.00"),
+                                  debit=wallet,
+                                  credit=bank)
+
+        response = self.client.get('/')
+        self.assertNotContains(response, "123,00")
